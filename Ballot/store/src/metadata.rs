73970419386d4c6ce88df082688e5b575071800c
chavic <chabundavictor@gmail.com>
2022-07-18 18:59:25 +0200

    metadata defined

--- Ballot/store/src/metadata.rs
+++ Ballot/store/src/metadata.rs
@@ -1,5 +1,32 @@
-pub struct Metadata {}
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::fs::{read_to_string, File};
+use std::io::Result;
+use std::io::Write;
+use std::path::{Path, PathBuf};
+
+type JSON = Value;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Metadata {
+    meta: JSON,
+}
 
 pub enum SearchMetadata {
     TimeRange,
+}
+
+impl Metadata {
+    pub fn new(path: &Path) -> Result<Self> {
+        let file_string = read_to_string(path)?;
+        let metadata: Metadata = serde_json::from_str(&file_string)?;
+        Ok(metadata)
+    }
+
+    pub fn save(&self, path: &Path) -> Result<()> {
+        let file_string = serde_json::to_string(self)?;
+        let mut file = File::create(path)?;
+        write!(file, "{}", file_string)?;
+        Ok(())
+    }
 }
