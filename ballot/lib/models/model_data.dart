8309848f8e0d0ff8f8ec98104d650bb69d5b182e
seokamela <ntjobokoane@gmail.com>
2022-07-18 12:27:30 +0200

    Search function

--- ballot/lib/models/model_data.dart
+++ ballot/lib/models/model_data.dart
@@ -24,4 +24,34 @@ const candidates = [
     urlImage:
         'https://images-na.ssl-images-amazon.com/images/I/41d1gVUK1yL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
   ),
+  Candidate(
+    name: 'Just a test',
+    urlImage:
+        'https://images-na.ssl-images-amazon.com/images/I/41d1gVUK1yL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
+  ),
+  Candidate(
+    name: 'aaa a test',
+    urlImage:
+        'https://images-na.ssl-images-amazon.com/images/I/41HeGBk6FhL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
+  ),
+  Candidate(
+    name: 'Just a test',
+    urlImage:
+        'https://images-na.ssl-images-amazon.com/images/I/41d1gVUK1yL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
+  ),
+  Candidate(
+    name: 'Just a test',
+    urlImage:
+        'https://images-na.ssl-images-amazon.com/images/I/41d1gVUK1yL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
+  ),
+  Candidate(
+    name: 'aaa a test',
+    urlImage:
+        'https://images-na.ssl-images-amazon.com/images/I/41HeGBk6FhL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
+  ),
+  Candidate(
+    name: 'Just a test',
+    urlImage:
+        'https://images-na.ssl-images-amazon.com/images/I/41d1gVUK1yL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg',
+  ),
 ];
