091baa408a35e8fac6475ea03a877772d2d8ad04
chavic <chabundavictor@gmail.com>
2022-07-19 05:55:11 +0200

    defined chain impl block

--- Ballot/store/src/chain.rs
+++ Ballot/store/src/chain.rs
@@ -1,7 +1,6 @@
 use std::fs::File;
 
 use crate::block::*;
-use crate::metadata;
 use crate::metadata::*;
 
 pub struct Chain<H: Hashable, T: Hashable + Datable> {
@@ -16,5 +15,25 @@ impl Chain<String, File> {
 
     pub fn build_seg(metadata: Metadata, start: usize, end: usize) -> Chain<String, File> {
         todo!("Imblicment block segment")
+    }
+
+    pub fn add_block(&self, block: Block<String, File>) {
+        todo!("add block")
+    }
+
+    pub fn find_block_with_hash_seg(
+        &self,
+        hash_seg: String,
+        search: SearchMetadata,
+    ) -> Result<Option<Block<String, File>>> {
+        todo!("Find block with has")
+    }
+
+    pub fn find_hash_with_seg(
+        &self,
+        hash_seg: String,
+        block: Option<Block<String, File>>,
+    ) -> Result<Option<String>> {
+        todo!("hash seg")
     }
 }
